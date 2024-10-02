a64548784b842b866a0aab663203453bb3354f05
Shivam Dubey <118355740+shivam969619@users.noreply.github.com>
2024-10-02 15:27:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Skills from "@/components/Skills"
 import ProjectSection from "@/components/ProjectSection"
 import ContactForm from "@/components/ContactForm"
 
-export default function page() {
+export default function Page() {
   return (
     <div>
       <div className="bg-[#09001f] w-full h-full text-white">
